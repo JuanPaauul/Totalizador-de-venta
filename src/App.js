5b8382cfd93cba2d02d9399382d4016f6847b597
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-09-08 19:38:25 -0400

    Calcular descuento

--- src/App.js
+++ src/App.js
@@ -30,9 +30,37 @@ function calculateSubTot(priceTot,state){
             tax = 0.0825;
             break;
         default:
+            
             break;
     }
     return priceTot+(priceTot*tax);
+}
+/*
+            1000            	3%
+        	3000            	5%
+        	7000	        	7%
+        	10000          	    10%
+        	30000          	    15%
+*/
+function calculateDiscount(total)
+{
+    var discount = 0;
+    if(total>= 30000){
+        discount = 0.15;
+    }
+    else if(total>= 10000){
+        discount = 0.10;
+    }
+    else if(total>= 7000){
+        discount = 0.07;
+    }
+    else if(total>= 3000){
+        diacaunt = 0.05;
+    }
+    else if(total>= 1000){
+        discount = 0.03;
+    }
+    return (total*discount);
 }
 form.addEventListener("submit", (event) => {
 
@@ -40,11 +68,13 @@ form.addEventListener("submit", (event) => {
     var msgPrice = "El precio del producto es: ";
     var msgTotalprice= "El precio total del pedido sin impuestos ni descuentos es: "
     var msgTax = "El precio Subtotal con impuesto es: "
+    var msgDiscaunt ="El descuento por su compra es : "
     var tot = calculateTot(cant.value,price.value);
     var subTot = calculateSubTot(tot,state.value);
-    alert(msg + cant.value +"\n" + msgPrice + price.value+ "\n"+ msgTotalprice+tot +"\n" +msgTax+subTot);
-
-    
-
-
+    var discountTot = calculateDiscount(subTot);
+    alert(msg + cant.value + "\n" 
+        + msgPrice + price.value+ "\n"
+        + msgTotalprice + tot + "\n"
+        + msgTax + subTot + "\n"
+        + msgDiscaunt + discountTot);
 });
