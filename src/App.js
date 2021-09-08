3ce7cc99bf9e49e1e8a6f1fa9465712f4be1fd69
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-09-08 19:11:29 -0400

    Calcular precio total sin descuentos ni impuestos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 const form = document.querySelector("#register");
 const cant = document.querySelector("#cant-items");
 const price = document.querySelector("#precio-items");
-
+function calculateTot(a,b){
+    return a*b;
+}
 form.addEventListener("submit", (event) => {
 
     var msg= "La cantidad registrada de items es: ";
     var msgPrice = "El precio del producto es: ";
-    alert(msg + cant.value +"\n" + msgPrice + price.value);
+    var msgTotalprice= "El precio total del pedido sin impuestos ni descuentos es: "
+    
+    alert(msg + cant.value +"\n" + msgPrice + price.value+ "\n"+ msgTotalprice+calculateTot(cant.value,price.value));
+
     
 
 
