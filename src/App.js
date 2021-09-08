62a726deb7be03236a5cb3233697b420f0bd8948
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-09-08 19:07:12 -0400

    Mostrar precio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 const form = document.querySelector("#register");
 const cant = document.querySelector("#cant-items");
-
+const price = document.querySelector("#precio-items");
 
 form.addEventListener("submit", (event) => {
 
     var msg= "La cantidad registrada de items es: ";
-    
-    alert(msg + cant.value  );
+    var msgPrice = "El precio del producto es: ";
+    alert(msg + cant.value +"\n" + msgPrice + price.value);
     
 
 
